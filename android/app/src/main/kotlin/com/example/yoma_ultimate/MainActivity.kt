d70641583c69d94b9aa893a53f67cf30ef101e7c
Patricia Giorgetto <patriciagiorgetto@hotmail.com>
2025-11-22 01:42:16 -0300

    Fix: remove android v1 embedding leftovers

--- android/app/src/main/kotlin/com/example/yoma_ultimate/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.yoma_ultimate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/yoma/app/MainActivity.kt
@@ -0,0 +1,8 @@
+Set-Content -Path "android/app/src/main/kotlin/com/yoma/app/MainActivity.kt" -Value @"
+package com.yoma.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
+"@
