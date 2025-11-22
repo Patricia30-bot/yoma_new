a258143b3ca98c656252b33dc680f9fe6eab5dab
Patricia Giorgetto <patriciagiorgetto@hotmail.com>
2025-11-22 00:37:33 -0300

    Fix: update codemagic.yaml and clean andoid V2 embedding

---
+++ android/app/src/main/kotlin/com/yoma/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yoma.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/yoma/yoma_ultimate/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.yoma.yoma_ultimate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
